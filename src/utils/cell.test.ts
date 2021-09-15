f20798a275011310538b947eaf73c73ca5f1a45c
t-blackwell <tlb.blackwell@googlemail.com>
2021-09-15 08:41:17 +0100

    tests for cell utils

---
+++ src/utils/cell.test.ts
@@ -0,0 +1,601 @@
+import { Char } from 'interfaces';
+import { revealGrid as cellsActionRevealGrid } from 'redux/cellsSlice';
+import invalidData1 from 'testData/test.invalid.1';
+import invalidData2 from 'testData/test.invalid.2';
+import invalidData3 from 'testData/test.invalid.3';
+import invalidData4 from 'testData/test.invalid.4';
+import invalidData5 from 'testData/test.invalid.5';
+import validData from 'testData/test.valid.1';
+import { mergeCell, blankNeighbours, initialiseCells } from './cell';
+import { initialiseGuessGrid } from './guess';
+import { store } from './rtl';
+import { initialiseStore } from './test';
+
+test('mergeCell', () => {
+  initialiseStore(store, validData);
+  const { cells } = store.getState().cells;
+  const newCell = {
+    clueIds: [],
+    groupAcross: [],
+    groupDown: [],
+    guess: undefined,
+    num: 1,
+    pos: { col: 0, row: 0 },
+    selected: false,
+    val: 'X' as Char,
+  };
+  const mergedCells = mergeCell(newCell, cells);
+  expect(mergedCells).toEqual(expect.arrayContaining([newCell]));
+});
+
+test('mergeCell with invalid cell position', () => {
+  initialiseStore(store, validData);
+  const { cells } = store.getState().cells;
+  const newCell = {
+    clueIds: [],
+    groupAcross: [],
+    groupDown: [],
+    guess: undefined,
+    num: 1,
+    pos: { col: 999, row: 999 },
+    selected: false,
+    val: 'X' as Char,
+  };
+  const mergedCells = mergeCell(newCell, cells);
+  expect(mergedCells).not.toEqual(expect.arrayContaining([newCell]));
+});
+
+test('blankNeighbours with empty cells', () => {
+  initialiseStore(store, validData);
+
+  const { cells } = store.getState().cells;
+  const currentCell = cells.find(
+    (cell) => cell.pos.col === 0 && cell.pos.row === 0,
+  );
+  expect(currentCell).toBeDefined();
+  if (currentCell === undefined) {
+    return;
+  }
+
+  const hasBlankNeighbours = blankNeighbours(cells, currentCell, true);
+  expect(hasBlankNeighbours).toBeTruthy();
+});
+
+test('blankNeighbours with neighbours', () => {
+  initialiseStore(store, validData);
+  store.dispatch(cellsActionRevealGrid());
+
+  const { cells } = store.getState().cells;
+  const currentCell = cells.find(
+    (cell) => cell.pos.col === 0 && cell.pos.row === 0,
+  );
+  expect(currentCell).toBeDefined();
+  if (currentCell === undefined) {
+    return;
+  }
+
+  const hasBlankNeighbours = blankNeighbours(cells, currentCell, true);
+  expect(hasBlankNeighbours).toBeFalsy();
+});
+
+test('blankNeighbours without neighbours', () => {
+  initialiseStore(store, validData);
+  store.dispatch(cellsActionRevealGrid());
+
+  const { cells } = store.getState().cells;
+  const currentCell = cells.find(
+    (cell) => cell.pos.col === 1 && cell.pos.row === 0,
+  );
+  expect(currentCell).toBeDefined();
+  if (currentCell === undefined) {
+    return;
+  }
+
+  const hasBlankNeighbours = blankNeighbours(cells, currentCell, true);
+  expect(hasBlankNeighbours).toBeTruthy();
+});
+
+test('initialiseCells with invalid data 1', () => {
+  expect(() =>
+    initialiseCells(
+      invalidData1.dimensions.cols,
+      invalidData1.dimensions.rows,
+      invalidData1.entries,
+    ),
+  ).toThrow('Crossword data error: solution length mismatch');
+});
+
+test('initialiseCells with invalid data 2', () => {
+  expect(() =>
+    initialiseCells(
+      invalidData2.dimensions.cols,
+      invalidData2.dimensions.rows,
+      invalidData2.entries,
+    ),
+  ).toThrow('Crossword data error: out of bounds');
+});
+
+test('initialiseCells with invalid data 3', () => {
+  expect(() =>
+    initialiseCells(
+      invalidData3.dimensions.cols,
+      invalidData3.dimensions.rows,
+      invalidData3.entries,
+    ),
+  ).toThrow('Crossword data error: solution character clash');
+});
+
+test('initialiseCells with invalid data 4', () => {
+  expect(() =>
+    initialiseCells(
+      invalidData4.dimensions.cols,
+      invalidData4.dimensions.rows,
+      invalidData4.entries,
+    ),
+  ).toThrow('Crossword data error: overlapping across solutions');
+});
+
+test('initialiseCells with invalid data 5', () => {
+  expect(() =>
+    initialiseCells(
+      invalidData5.dimensions.cols,
+      invalidData5.dimensions.rows,
+      invalidData5.entries,
+    ),
+  ).toThrow('Crossword data error: overlapping down solutions');
+});
+
+test('initialiseCells with valid data', () => {
+  const cells = initialiseCells(
+    validData.dimensions.cols,
+    validData.dimensions.rows,
+    validData.entries,
+  );
+
+  expect(cells.length).toBe(23);
+  expect(cells).toEqual([
+    {
+      clueIds: ['1-across', '1-down'],
+      guess: undefined,
+      num: 1,
+      pos: { col: 0, row: 0 },
+      selected: false,
+      val: 'Y',
+      groupAcross: ['1-across'],
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 1, row: 0 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['1-across'],
+    },
+    {
+      clueIds: ['1-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 2, row: 0 },
+      selected: false,
+      val: 'Y',
+      groupAcross: ['1-across'],
+    },
+    {
+      clueIds: ['1-across', '2-down'],
+      guess: undefined,
+      num: 2,
+      pos: { col: 3, row: 0 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['1-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['4-across', '1-down'],
+      guess: undefined,
+      num: 4,
+      pos: { col: 0, row: 2 },
+      selected: false,
+      val: 'L',
+      groupAcross: ['4-across'],
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 1, row: 2 },
+      selected: false,
+      val: 'I',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 2, row: 2 },
+      selected: false,
+      val: 'E',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across', '2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 2 },
+      selected: false,
+      val: 'D',
+      groupAcross: ['4-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 4, row: 2 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 5, row: 2 },
+      selected: false,
+      val: 'W',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across', '3-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 6, row: 2 },
+      selected: false,
+      val: 'N',
+      groupAcross: ['4-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 0, row: 1 },
+      selected: false,
+      val: 'E',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 0, row: 3 },
+      selected: false,
+      val: 'L',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 0, row: 4 },
+      selected: false,
+      val: 'O',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 0, row: 5 },
+      selected: false,
+      val: 'W',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 1 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 3 },
+      selected: false,
+      val: 'S',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 4 },
+      selected: false,
+      val: 'A',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 5 },
+      selected: false,
+      val: 'N',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 3, row: 6 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: undefined,
+      num: 3,
+      pos: { col: 6, row: 1 },
+      selected: false,
+      val: 'E',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 6, row: 3 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: undefined,
+      num: undefined,
+      pos: { col: 6, row: 4 },
+      selected: false,
+      val: 'S',
+      groupDown: ['2-down', '3-down'],
+    },
+  ]);
+});
+
+test('initialiseCells with valid data and guess grid', () => {
+  const gridChar = 'X';
+  const guessGrid = initialiseGuessGrid(
+    validData.dimensions.cols,
+    validData.dimensions.rows,
+    gridChar,
+  );
+  const cells = initialiseCells(
+    validData.dimensions.cols,
+    validData.dimensions.rows,
+    validData.entries,
+    guessGrid,
+  );
+
+  expect(cells.length).toBe(23);
+  expect(cells).toEqual([
+    {
+      clueIds: ['1-across', '1-down'],
+      guess: gridChar,
+      num: 1,
+      pos: { col: 0, row: 0 },
+      selected: false,
+      val: 'Y',
+      groupAcross: ['1-across'],
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 1, row: 0 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['1-across'],
+    },
+    {
+      clueIds: ['1-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 2, row: 0 },
+      selected: false,
+      val: 'Y',
+      groupAcross: ['1-across'],
+    },
+    {
+      clueIds: ['1-across', '2-down'],
+      guess: gridChar,
+      num: 2,
+      pos: { col: 3, row: 0 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['1-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['4-across', '1-down'],
+      guess: gridChar,
+      num: 4,
+      pos: { col: 0, row: 2 },
+      selected: false,
+      val: 'L',
+      groupAcross: ['4-across'],
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 1, row: 2 },
+      selected: false,
+      val: 'I',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 2, row: 2 },
+      selected: false,
+      val: 'E',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across', '2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 2 },
+      selected: false,
+      val: 'D',
+      groupAcross: ['4-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 4, row: 2 },
+      selected: false,
+      val: 'O',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 5, row: 2 },
+      selected: false,
+      val: 'W',
+      groupAcross: ['4-across'],
+    },
+    {
+      clueIds: ['4-across', '3-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 6, row: 2 },
+      selected: false,
+      val: 'N',
+      groupAcross: ['4-across'],
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 0, row: 1 },
+      selected: false,
+      val: 'E',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 0, row: 3 },
+      selected: false,
+      val: 'L',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 0, row: 4 },
+      selected: false,
+      val: 'O',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['1-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 0, row: 5 },
+      selected: false,
+      val: 'W',
+      groupDown: ['1-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 1 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 3 },
+      selected: false,
+      val: 'S',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 4 },
+      selected: false,
+      val: 'A',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 5 },
+      selected: false,
+      val: 'N',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['2-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 3, row: 6 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: gridChar,
+      num: 3,
+      pos: { col: 6, row: 1 },
+      selected: false,
+      val: 'E',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 6, row: 3 },
+      selected: false,
+      val: 'D',
+      groupDown: ['2-down', '3-down'],
+    },
+    {
+      clueIds: ['3-down'],
+      guess: gridChar,
+      num: undefined,
+      pos: { col: 6, row: 4 },
+      selected: false,
+      val: 'S',
+      groupDown: ['2-down', '3-down'],
+    },
+  ]);
+});

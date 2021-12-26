eb8052b1b43316dc1b592b980fe7c795911c945a
Tom Blackwell <31444631+t-blackwell@users.noreply.github.com>
2021-12-26 10:51:09 +0000

    added previous/next to StickyClue (#25)
    * added buttons and props to StickyClue
    
    * added svg icon to StickyClue buttons
    
    * added tests

--- src/components/StickyClue/StickyClue.test.tsx
+++ src/components/StickyClue/StickyClue.test.tsx
@@ -1,12 +1,59 @@
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import StickyClue from './StickyClue';
 
 test('it renders', () => {
-  render(<StickyClue num="num" text="one & <strong>two</strong>" />);
+  render(
+    <StickyClue
+      num="num"
+      onMoveNext={jest.fn}
+      onMovePrev={jest.fn}
+      text="one & <strong>two</strong>"
+    />,
+  );
   screen.getByText('num');
   screen.getByText(/one &/i);
   const two = screen.getByText('two');
   expect(two.tagName).toBe('STRONG');
+
+  const buttons = screen.getAllByRole('button');
+  expect(buttons.length).toBe(2);
+});
+
+test('it renders empty', () => {
+  render(<StickyClue onMoveNext={jest.fn} onMovePrev={jest.fn} />);
+  const buttons = screen.queryAllByRole('button');
+  expect(buttons.length).toBe(0);
+});
+
+test('it calls onMoveNext', () => {
+  const onMove = jest.fn();
+  render(
+    <StickyClue
+      num="num"
+      onMoveNext={onMove}
+      onMovePrev={jest.fn}
+      text="text"
+    />,
+  );
+  const button = screen.getByRole('button', { name: /next clue/i });
+  userEvent.click(button);
+  expect(onMove).toHaveBeenCalledTimes(1);
+});
+
+test('it calls onMovePrev', () => {
+  const onMove = jest.fn();
+  render(
+    <StickyClue
+      num="num"
+      onMoveNext={jest.fn}
+      onMovePrev={onMove}
+      text="text"
+    />,
+  );
+  const button = screen.getByRole('button', { name: /previous clue/i });
+  userEvent.click(button);
+  expect(onMove).toHaveBeenCalledTimes(1);
 });
--- src/components/StickyClue/StickyClue.tsx
+++ src/components/StickyClue/StickyClue.tsx
@@ -1,23 +1,85 @@
 import * as React from 'react';
 import { sanitizeHtml } from './../../utils/general';
 
+interface ChevronIconProps {
+  className?: string;
+}
+
+function ChevronLeftIcon({ className }: ChevronIconProps): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      className={className}
+      viewBox="0 0 16 16"
+    >
+      <path
+        fillRule="evenodd"
+        d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+      />
+    </svg>
+  );
+}
+
+function ChevronRightIcon({ className }: ChevronIconProps): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      className={className}
+      viewBox="0 0 16 16"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+      />
+    </svg>
+  );
+}
+
 interface StickyClueProps {
-  num: string;
-  text: string;
+  num?: string;
+  onMoveNext: () => void;
+  onMovePrev: () => void;
+  text?: string;
 }
 
 export default function StickyClue({
   num,
+  onMoveNext,
+  onMovePrev,
   text,
 }: StickyClueProps): JSX.Element {
   return (
     <div className="StickyClue">
-      <div className="StickyClue__inner">
-        <span className="StickyClue__text">
-          <span className="StickyClue__num">{num}</span>
-          <span dangerouslySetInnerHTML={{ __html: sanitizeHtml(text) }} />
-        </span>
-      </div>
+      {text !== undefined && num !== undefined ? (
+        <>
+          <button
+            aria-label="Previous clue"
+            className="StickyClue__button"
+            onClick={onMovePrev}
+            type="button"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <div className="StickyClue__inner">
+            <span className="StickyClue__text">
+              <span className="StickyClue__num">{num}</span>
+              <span dangerouslySetInnerHTML={{ __html: sanitizeHtml(text) }} />
+            </span>
+          </div>
+          <button
+            aria-label="Next clue"
+            className="StickyClue__button"
+            onClick={onMoveNext}
+            type="button"
+          >
+            <ChevronRightIcon />
+          </button>
+        </>
+      ) : null}
     </div>
   );
 }

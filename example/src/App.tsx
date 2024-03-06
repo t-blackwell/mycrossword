817abbf4100449be5ad2b095f3b6be268a4de24b
tblackwell-tm <t.blackwell@techmodal.com>
2024-03-06 10:47:25 +0000

    incomplete grid

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import MyCrossword from 'mycrossword';
 import 'mycrossword/dist/index.css';
 import * as React from 'react';
 import './App.css';
-import data from './guardian.cryptic.28505';
+import data from './incomplete.cryptic';
 
 // TODO: import from MyCrossword
 const themeColors = [

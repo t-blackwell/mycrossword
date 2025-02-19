a75f3100c9cae378701283240a21051b2eb33863
tblackwell-tm <t.blackwell@techmodal.com>
2025-02-19 06:51:49 +0000

    Vite config adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import { extname, relative, resolve } from 'path';
-import { fileURLToPath } from 'node:url';
-import { glob } from 'glob';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
@@ -20,31 +18,15 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es'],
+      fileName: 'main',
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        // https://rollupjs.org/configuration-options/#input
-        glob
-          .sync('lib/**/*.{ts,tsx}', {
-            ignore: [
-              'lib/**/*.d.ts',
-              'lib/**/*.test.{ts,tsx}',
-              'lib/testData/**',
-            ],
-          })
-          .map((file) => [
-            // 1. The name of the entry point
-            // lib/nested/foo.js becomes nested/foo
-            relative('lib', file.slice(0, file.length - extname(file).length)),
-            // 2. The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ]),
-      ),
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
       output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
+        // Force CSS code-splitting off
+        manualChunks: undefined,
+        // Ensure CSS is properly processed when code-split
+        inlineDynamicImports: true,
       },
     },
   },
